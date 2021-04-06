c950e6482e3a76b462e1c1fab90f86fdbca1d399
Arkadiusz (Eric) <35996962+S00182038@users.noreply.github.com>
2021-04-06 19:12:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Article Organiser Application
         </a>
       </header>
     </div>
